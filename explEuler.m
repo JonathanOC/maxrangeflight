2d586ad8697d2def7c0148788347290d10bb5f22
Jonathan Z <jonathan.zoller@arcor.de>
2015-05-15 14:11:00 +0200

    energyconservation added

--- explEuler.m
+++ explEuler.m
@@ -5,6 +5,6 @@ h = (te-ta)/N;
 y = zeros(numel(ya),N+1);
 y(:,1) = ya;
 for i = 1:N
-    dy = f(t(i), y(:,i))';
+    dy = f(t(i), y(:,i));
     y(:,i+1) = y(:,i) + h*dy;
 end
--- test.m
+++ test.m
@@ -1,5 +1,5 @@
-clear all;
-close all;
+%clear all;
+%close all;
 
 
 global param;
